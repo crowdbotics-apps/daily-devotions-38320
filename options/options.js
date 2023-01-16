27896c7045cfca950dbb55941b8d12048eb22174
Crowdbotics <team@crowdbotics.com>
2023-01-16 19:25:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "daily-devotions-38320",
+  url: "https://daily-devotions-38320.botics.co",
+  api: "https://daily-devotions-38320.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
